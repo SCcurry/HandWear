23214aff99c5c5e880e7a2634beba91c6c3fec78
curry <1812584700@qq.com>
2017-12-21 21:40:42 +0800

    实现设备的到扫描功能等
    。。。脑袋爆炸

--- app/src/main/java/com/example/administrator/handwear/MainActivity.java
+++ app/src/main/java/com/example/administrator/handwear/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.administrator.handwear;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothManager;
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,9 +25,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button saomiao , duanzhen , changzhen , buting , tingxia;
     private TextView jibu , dianliang , lianjiezhuangtai;
+    private ListView list;
 
     BluetoothAdapter bluetoothAdapter;
-    List<BluetoothDevice> deviceList = new ArrayList<>();//用一个数组来保存扫描到的设备
+    BluetoothGatt bluetoothGatt;
+    List<BluetoothDevice> deviceList = new ArrayList<>();
+    BluetoothDevice bluetoothDevice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         changzhen = (Button) findViewById(R.id.changzhen);
         buting = (Button) findViewById(R.id.buting);
         tingxia = (Button) findViewById(R.id.tingxia);
+        list = (ListView) findViewById(R.id.list);
 
         jibu = (TextView) findViewById(R.id.jibu);
         dianliang = (TextView) findViewById(R.id.dianliang);
@@ -54,6 +64,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buting.setOnClickListener(this);
         tingxia.setOnClickListener(this);
 
+        //item 监听事件
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                bluetoothDevice = deviceList.get(i);
+                //连接设备的方法,返回值为bluetoothgatt类型
+                bluetoothGatt = bluetoothDevice.connectGatt(MainActivity.this, false, gattcallback);
+                lianjiezhuangtai.setText("连接" + bluetoothDevice.getName() + "中...");
+            }
+        });
 
     }
 
@@ -74,6 +94,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }
                 });
                 scanThread.start();
+
                 break;
             case R.id.zhendong:
 
@@ -87,17 +108,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.tingxia:
 
                 break;
+            case R.id.list:
+
+                break;
 
         }
     }
+
     public void saomiao(){
         deviceList.clear();
-
         bluetoothAdapter.startLeScan(callback);
     }
 
-    public BluetoothAdapter.LeScanCallback callback = new BluetoothAdapter.LeScanCallback()
-    {
+    //扫描回调
+    public BluetoothAdapter.LeScanCallback callback = new BluetoothAdapter.LeScanCallback() {
         @Override
         public void onLeScan(final BluetoothDevice bluetoothDevice, int i, byte[] bytes) {
             Log.i("TAG", "onLeScan: " + bluetoothDevice.getName() + "/t" + bluetoothDevice.getAddress() + "/t" + bluetoothDevice.getBondState());
@@ -106,9 +130,91 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (!deviceList.contains(bluetoothDevice)) {
                 //将设备加入列表数据中
                 deviceList.add(bluetoothDevice);
+
+                list.setAdapter(new MyAdapter(MainActivity.this , deviceList));
             }
 
         }
     };
 
+    private BluetoothGattCallback gattcallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, final int newState) {
+            super.onConnectionStateChange(gatt, status, newState);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    String status;
+                    switch (newState) {
+                        //已经连接
+                        case BluetoothGatt.STATE_CONNECTED:
+                            lianjiezhuangtai.setText("已连接");
+
+                            //该方法用于获取设备的服务，寻找服务
+                            bluetoothGatt.discoverServices();
+                            break;
+                        //正在连接
+                        case BluetoothGatt.STATE_CONNECTING:
+                            lianjiezhuangtai.setText("正在连接");
+                            break;
+                        //连接断开
+                        case BluetoothGatt.STATE_DISCONNECTED:
+                            lianjiezhuangtai.setText("已断开");
+                            break;
+                        //正在断开
+                        case BluetoothGatt.STATE_DISCONNECTING:
+                            lianjiezhuangtai.setText("断开中");
+                            break;
+                    }
+                    //pd.dismiss();
+                }
+            });
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            super.onServicesDiscovered(gatt, status);
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicRead(gatt, characteristic, status);
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicWrite(gatt, characteristic, status);
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            super.onCharacteristicChanged(gatt, characteristic);
+        }
+
+        @Override
+        public void onDescriptorRead(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+            super.onDescriptorRead(gatt, descriptor, status);
+        }
+
+        @Override
+        public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+            super.onDescriptorWrite(gatt, descriptor, status);
+        }
+
+        @Override
+        public void onReliableWriteCompleted(BluetoothGatt gatt, int status) {
+            super.onReliableWriteCompleted(gatt, status);
+        }
+
+        @Override
+        public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status) {
+            super.onReadRemoteRssi(gatt, rssi, status);
+        }
+
+        @Override
+        public void onMtuChanged(BluetoothGatt gatt, int mtu, int status) {
+            super.onMtuChanged(gatt, mtu, status);
+        }
+    } ;
 }
